b616709a7c681d39bf779cc816e4c5aab07b38d7
Justin Smestad <justin.smestad@gmail.com>
2018-05-28 22:35:44 -0600

    Indent for HTML/CSS

--- .emacs.d/vendor/js-web.el
+++ .emacs.d/vendor/js-web.el
@@ -9,7 +9,8 @@
 
 (use-package web-mode
   :mode
-  (("\\.erb\\'"        . web-mode)
+  (("\\.html\\'"       . web-mode)
+   ("\\.erb\\'"        . web-mode)
    ("\\.php\\'"        . web-mode)
    ("\\.hbs\\'"        . web-mode)
    ("\\.handlebars\\'" . web-mode)
@@ -22,6 +23,9 @@
   ;;       ("," . self-with-space)
   ;;       ("<C-return>" . html-newline-dwim))
   :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
   (web-mode-enable-auto-quoting nil)
   (web-mode-enable-current-element-highlight t))
 (use-package company-web
