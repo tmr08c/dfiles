e6ba3cb015d10368f193e84b5e31192c4f91e5e3
Justin Smestad <justin.smestad@gmail.com>
2018-08-07 06:47:22 -0600

    Spike on java emacs configuration

---
+++ .emacs.d/vendor/js-java.el
@@ -0,0 +1,19 @@
+;;; js-java.el --- Part of my Emacs configuration
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package eclimd
+  :disabled
+  :hook (java-mode . eclim-mode))
+
+(use-package company-emacs-eclim
+  :disabled
+  :hook company
+  :hook (java-mode . company-emacs-eclim-setup))
+
+
+(provide 'js-java)
+
+;;; js-java.el ends here
