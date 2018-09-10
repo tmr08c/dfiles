0364b898ce884f01c861b9aa05ec29982b8fdbb5
Justin Smestad <justin.smestad@gmail.com>
2018-09-10 10:32:06 -0600

    Add basic doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+(setq doom-font (font-spec :family "Fira Mono" :size 13)
+      doom-variable-pitch-font (font-spec :family "Fira Sans")
+      doom-theme 'doom-molokai
+      doom-big-font (font-spec :family "Fira Mono" :size 19))
