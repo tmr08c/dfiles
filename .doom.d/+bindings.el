2bb20ca11439f12d3e597c21445fe263d937b274
Justin Smestad <justin.smestad@gmail.com>
2018-10-09 14:11:53 -0600

    Neotree back to my normal defaults

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -86,10 +86,10 @@
       :n  "[b" #'previous-buffer
       :m  "]o" #'outline-next-visible-heading
       :m  "[o" #'outline-previous-visible-heading
-      :n  "]w" #'+workspace/switch-right
-      :n  "[w" #'+workspace/switch-left
-      :m  "gt" #'+workspace/switch-right
-      :m  "gT" #'+workspace/switch-left
+      ;; :n  "]w" #'+workspace/switch-right
+      ;; :n  "[w" #'+workspace/switch-left
+      ;; :m  "gt" #'+workspace/switch-right
+      ;; :m  "gT" #'+workspace/switch-left
       :m  "gd" #'+lookup/definition
       :m  "gD" #'+lookup/references
       :n  "gf" #'+lookup/file
@@ -404,29 +404,31 @@
       ;; neotree
       (:after neotree
         :map neotree-mode-map
-        :n "g"         nil
-        :n [tab]       #'neotree-quick-look
-        :n "RET"       #'neotree-enter
-        :n [backspace] #'evil-window-prev
-        :n "c"         #'neotree-create-node
-        :n "r"         #'neotree-rename-node
-        :n "d"         #'neotree-delete-node
-        :n "j"         #'neotree-next-line
-        :n "k"         #'neotree-previous-line
-        :n "n"         #'neotree-next-line
-        :n "p"         #'neotree-previous-line
-        :n "h"         #'+neotree/collapse-or-up
-        :n "l"         #'+neotree/expand-or-open
-        :n "J"         #'neotree-select-next-sibling-node
-        :n "K"         #'neotree-select-previous-sibling-node
-        :n "H"         #'neotree-select-up-node
-        :n "L"         #'neotree-select-down-node
-        :n "G"         #'evil-goto-line
-        :n "gg"        #'evil-goto-first-line
-        :n "v"         #'neotree-enter-vertical-split
-        :n "s"         #'neotree-enter-horizontal-split
-        :n "q"         #'neotree-hide
-        :n "R"         #'neotree-refresh)
+        :n "RET" #'neotree-enter
+        :n "TAB" #'neotree-stretch-toggle
+        :n "q" #'neotree-hide
+        :n "|" #'neotree-enter-vertical-split
+        :n "-" #'neotree-enter-horizontal-split
+        :n "'" #'neotree-quick-look
+        :n "c" #'neotree-create-node
+        :n "C" #'neotree-copy-node
+        :n "d" #'neotree-delete-node
+        :n "gr" #'neotree-refresh
+
+        :n "h" #'+neotree/collapse-or-up
+        :n "H" #'neotree-select-previous-sibling-node
+        :n "j" #'neotree-next-line
+        :n "J" #'neotree-select-down-node
+        :n "k" #'neotree-previous-line
+        :n "K" #'neotree-select-up-node
+        :n "L" #'neotree-select-next-sibling-node
+
+        :n "l" #'+neotree/expand-or-open
+        :n "q" #'neotree-hide
+        :n "o" #'neotree-enter
+        :n "r" #'neotree-rename-node
+        :n "R" #'neotree-change-root
+        :n "I" #'neotree-hidden-file-toggle)
 
       ;; realgud
       (:after realgud
