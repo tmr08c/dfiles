66e01560afa46c5d930975ce57b3494a9cd02846
Justin Smestad <justin.smestad@gmail.com>
2018-10-18 15:07:42 -0600

    More literacy

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -6,6 +6,19 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold 268435456)
 
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'.
 (setq package-enable-at-startup nil)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,36 @@
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
-(when (file-readable-p "~/.emacs.d/config.org")
+;;; init.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+
+;;; Code:
+
+;;; Add load path for vendor directory
+(add-to-list 'load-path (concat user-emacs-directory "vendor/"))
+
+;;; Get package repos configured
+(require 'package)
+(customize-set-variable
+ 'package-archives
+ '(("melpa" . "https://melpa.org/packages/")
+   ("gnu" . "https://elpa.gnu.org/packages/")
+   ("org" . "https://orgmode.org/elpa/")))
+(unless package--initialized
+  (package-initialize))
+
+;;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t
+      use-package-compute-statistics t)
+
+(eval-when-compile
+  (require 'use-package))
+
+;;; This is the actual configuration file.
+;;; It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p (concat user-emacs-directory "config.org"))
   (org-babel-load-file (concat user-emacs-directory "config.org")))
 
+(provide 'init)
+;;; init.el ends here
