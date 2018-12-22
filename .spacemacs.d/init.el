e53b3d2474dcbefee43828260f5fe6254f2db213
Justin Smestad <justin.smestad@gmail.com>
2018-12-22 15:07:10 -0700

    Fira Code change for Linux

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -251,7 +251,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Fira Code"
-                               :size 13)
+                               :size 13.0)
 
    ;; The leader key (default "SPC")
    dotspacemacs-leader-key "SPC"
