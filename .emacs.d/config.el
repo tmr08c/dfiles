8fd778fbe45623148a6823d4e77320b3f9f5a91c
Justin Smestad <justin.smestad@gmail.com>
2020-03-08 10:58:14 -0600

    Do not run Ember LSP in web mode by default

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -225,7 +225,7 @@ _q_ quit            _c_ create          _<_ previous
         lsp-keep-workspace-alive nil)
   (lsp-register-client
    (make-lsp-client :new-connection (lsp-stdio-connection (list "node" (concat (getenv "HOME") "/code/github/ember-language-server/lib/start-server.js") "--stdio"))
-                    :major-modes '(js-mode js2-mode web-mode)
+                    :major-modes '(js-mode js2-mode)
                     :priority -1
                     :ignore-messages (list "Initializing Ember Language Server at .*$")
                     :add-on? t
