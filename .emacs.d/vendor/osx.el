fb60217b1b5a21071ccf025bdc58dfae1a5491dd
Justin Smestad <justin.smestad@gmail.com>
2020-03-16 10:58:28 -0600

    Using Karabiner-Elements on macOS means no more custom keyboard mappings 🍾

--- .emacs.d/vendor/osx.el
+++ .emacs.d/vendor/osx.el
@@ -34,27 +34,27 @@
   :unless (boundp 'mac-system-move-file-to-trash-use-finder)
   :init (osx-trash-setup))
 
-(when (display-graphic-p)
-  ;; Treat command as super
-  (setq mac-command-key-is-meta nil
-        mac-command-modifier 'super
-        mac-option-key-is-meta 't
-        mac-option-modifier 'meta)
+;; (when (display-graphic-p)
+;;   ;; Treat command as super
+;;   (setq mac-command-key-is-meta nil
+;;         mac-command-modifier 'super
+;;         mac-option-key-is-meta 't
+;;         mac-option-modifier 'meta)
 
-  (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-  (global-set-key (kbd "s-v") 'yank)
-  (global-set-key (kbd "s-c") 'evil-yank)
-  (global-set-key (kbd "s-a") 'mark-whole-buffer)
-  (global-set-key (kbd "s-x") 'kill-region)
-  (global-set-key (kbd "s-w") 'delete-window)
-  (global-set-key (kbd "s-W") 'delete-frame)
-  (global-set-key (kbd "s-n") 'make-frame)
-  (global-set-key (kbd "s-z") 'undo-tree-undo)
-  (global-set-key (kbd "s-Z") 'undo-tree-redo)
-  (global-set-key (kbd "s-r") 'undo-tree-redo)
-  (global-set-key (kbd "s-s") (lambda ()
-                                (interactive)
-                                (call-interactively (key-binding "\C-x\C-s")))))
+;;   (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
+;;   (global-set-key (kbd "s-v") 'yank)
+;;   (global-set-key (kbd "s-c") 'evil-yank)
+;;   (global-set-key (kbd "s-a") 'mark-whole-buffer)
+;;   (global-set-key (kbd "s-x") 'kill-region)
+;;   (global-set-key (kbd "s-w") 'delete-window)
+;;   (global-set-key (kbd "s-W") 'delete-frame)
+;;   (global-set-key (kbd "s-n") 'make-frame)
+;;   (global-set-key (kbd "s-z") 'undo-tree-undo)
+;;   (global-set-key (kbd "s-Z") 'undo-tree-redo)
+;;   (global-set-key (kbd "s-r") 'undo-tree-redo)
+;;   (global-set-key (kbd "s-s") (lambda ()
+;;                                 (interactive)
+;;                                 (call-interactively (key-binding "\C-x\C-s")))))
 
 
 (provide 'osx)
