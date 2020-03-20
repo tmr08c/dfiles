364a135f30e8897e1af1d4523bd756e2551aba75
Justin Smestad <justin.smestad@gmail.com>
2020-03-19 21:23:52 -0600

    Disable Ember LSP server

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -223,13 +223,13 @@ _q_ quit            _c_ create          _<_ previous
         lsp-solargraph-diagnostics nil
         lsp-solargraph-rename nil
         lsp-keep-workspace-alive nil)
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection (list "node" (concat (getenv "HOME") "/code/github/ember-language-server/lib/start-server.js") "--stdio"))
-                    :major-modes '(js-mode js2-mode)
-                    :priority -1
-                    :ignore-messages (list "Initializing Ember Language Server at .*$")
-                    :add-on? t
-                    :server-id 'ember-ls))
+  ;; (lsp-register-client
+  ;;  (make-lsp-client :new-connection (lsp-stdio-connection (list "node" (concat (getenv "HOME") "/code/github/ember-language-server/lib/start-server.js") "--stdio"))
+  ;;                   :major-modes '(js-mode js2-mode)
+  ;;                   :priority -1
+  ;;                   :ignore-messages (list "Initializing Ember Language Server at .*$")
+  ;;                   :add-on? t
+  ;;                   :server-id 'ember-ls))
   (add-to-list 'exec-path "~/code/github/elixir-ls/release"))
 
 (use-package lsp-ui
