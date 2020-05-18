943af9aff696e24807112410bba7b649c167d1a0
Justin Smestad <justin.smestad@gmail.com>
2020-05-18 12:17:51 -0600

    Remove LSP for Ruby; Disable Editorconfig

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -85,7 +85,7 @@
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        direnv
        (docker +lsp)
-       editorconfig      ; let someone else argue about tabs vs spaces
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
@@ -153,7 +153,7 @@
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails +lsp)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
