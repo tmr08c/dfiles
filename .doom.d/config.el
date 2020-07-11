b8c3774553acd1cf118e520182ed6f96d1c06768
Justin Smestad <justin.smestad@gmail.com>
2020-07-11 17:03:17 -0600

    Allow file watchers for Elixir LS mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -30,6 +30,9 @@
 (after! projectile
   (setq! projectile-enable-caching (not (executable-find doom-projectile-fd-binary))))
 
+;; Elixir LSP keeps checking project every N seconds otherwise
+(setq lsp-enable-file-watchers t)
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
